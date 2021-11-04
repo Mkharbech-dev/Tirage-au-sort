dbbb541805c7cf47d70c591f84579040b5fed4ec
Quentin <wanheyda91@gmail.com>
2021-11-04 10:49:12 +0100

    Added persistance

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
 $(document).ready(function () {
     // initialisation du tableau vide servant au tirage au sort
-    var tableauDeBase = [];
+    var tableauDeBase = JSON.parse(localStorage.getItem('tableauDeBase'));
+    if(tableauDeBase == null)
+    {
+        tableauDeBase = [];
+    }
+    else
+    {
+        $(".tableauDeBase").html(tableauDeBase.join(" "));
+    }
 
+    
     // on ajoute les éléments qu'on souhaite au tableau qui va servir pour le tirage au sort quand on clique sur "ajouter"
     $("#addElement").click(function () {
         var elementTirage = $("#elementTirage").val();
         tableauDeBase.push(elementTirage);
+        console.log(tableauDeBase);
         $(".tableauDeBase").html(tableauDeBase.join(" "));
         clearForm();
+        console.log(tableauDeBase);
+        var myJsonString = JSON.stringify(tableauDeBase);
+        localStorage.setItem('tableauDeBase', myJsonString);
+        console.log(localStorage.getItem('tableauDeBase'));
     });
-    
+
+    $("#deleteElement").click(function () {
+        console.log(tableauDeBase.pop());
+        $(".tableauDeBase").html(tableauDeBase.join(" "));
+        var myJsonString = JSON.stringify(tableauDeBase);
+        localStorage.setItem('tableauDeBase', myJsonString);
+    });
+
     //fait comme un changement de page, la div de préparation du tirage disparait pour faire apparaitre 
     $("#goTirage").click(function (event) {
         event.preventDefault();
         $("#preparationTirage").addClass("d-none");
         $("#tiragePret").removeClass("d-none");
         $("#tiragePret").addClass("d-flex");
+        console.log(tableauDeBase);
+        var myJsonString = JSON.stringify(tableauDeBase);
+        localStorage.setItem('tableauDeBase', myJsonString);
+        console.log(localStorage.getItem('tableauDeBase'));
     })
 
     // fonction qui permet de vider le champ de saisie
@@ -31,15 +56,15 @@ $(document).ready(function () {
 
     //création de la fonction de tirage au sort sur l'ensemble du tableau
     function doRandom(tableau) {
-        return tableau[Math.floor(Math.random()*tableau.length)];
+        return tableau[Math.floor(Math.random() * tableau.length)];
     }
 
     //création de la fonction qui permet de faire sortir la donnée du tableau tirée au sort (indice) en la coupant
     //du tableau d'origine et en l'ajoutant dans le tableau vide
     function sortie(tableau, indice, tableau2) {
         for (var i = 0; i < tableau.length; i++) {
-            if(tableau[tableau.indexOf(indice)] == tableau[i]){
-                tableau.splice(tableau.indexOf(indice),1);
+            if (tableau[tableau.indexOf(indice)] == tableau[i]) {
+                tableau.splice(tableau.indexOf(indice), 1);
                 tableau2.push(indice)
             }
         }
@@ -47,32 +72,33 @@ $(document).ready(function () {
     }
     //création d'une fonction permettant de désactiver le bouton lancer quand le tableau de base est vide.
     function stopTirage(tableau) {
-        if(tableau.length == 0){
+        if (tableau.length == 0) {
             $("#lancer").hide();
         }
     }
-    
+
     //création de l'évènement: quand on clique sur le bouton "lancer", le compte à rebours de 3sec s'enclenche
     //et à sa fin (clearInterval) on annonce la personne tirée au sort, on met à jour le tableau de base,
     //et on affiche les personnes déjà tirées au sort avec le tableau vide sous forme de liste espacée de " ".
-    btnlancer.addEventListener("click", function(){
-            var timeleft = 3;
-            var downloadTimer = setInterval(function(){
-            if(timeleft <= 0){
+    btnlancer.addEventListener("click", function () {
+        var timeleft = 3;
+        var downloadTimer = setInterval(function () {
+            if (timeleft <= 0) {
                 clearInterval(downloadTimer);
                 document.getElementById("resultat").innerHTML = sortie(tableauDeBase, doRandom(tableauDeBase), tableauDeSortie);
                 stopTirage(tableauDeBase);
                 document.querySelector("#tableauInitial").innerHTML = tableauDeBase.join(" ");
-                document.querySelector("#elementsPicked").innerHTML = "Elements sortis: "+tableauDeSortie.join(" ");
+                document.querySelector("#elementsPicked").innerHTML = "Elements sortis: " + tableauDeSortie.join(" ");
             } else {
                 document.getElementById("resultat").innerHTML = timeleft;
             }
             timeleft -= 1;
-            }, 1000);
+        }, 1000);
     });
 
     // fonction pour recommencer le tirage avec les mêmes éléments. Même si on recommence en plein tirage alors qu'il reste des éléments, il reprend tous les éléments du départ MAIS pas dans le même ordre.
     $("#restart").click(function () {
+        console.log("toz");
         $("#lancer").show();
         tableauDeBase = tableauDeBase.concat(tableauDeSortie);
         tableauDeSortie = [];
@@ -80,4 +106,16 @@ $(document).ready(function () {
         $("#resultat").html("");
         $("#elementsPicked").html("");
     })
+
+    $("#nouveauTirage").click(function () {
+        tableauDeBase = [];
+        var myJsonString = JSON.stringify(tableauDeBase);
+        localStorage.setItem('tableauDeBase', myJsonString);
+        $("#lancer").show();
+        tableauDeBase = localStorage.getItem('tableauDeBase');
+        tableauDeSortie = [];
+        $("#resultat").html("");
+        $("#elementsPicked").html("");
+    })
+
 })
